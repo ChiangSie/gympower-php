49f1a7de98ddd6de1dd0e8d2e59558a23add3a62
ChiangSie <jimmy7369699@gmail.com>
2024-07-12 07:19:21 +0800

    更

---
+++ get_course_rating.php
@@ -0,0 +1,37 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
+try {
+    // 包含資料庫連接設定
+    require_once("connect_cid101g5.php");
+
+    // 設定返回資料的初始值
+    $returnData = [
+        'code' => 200,
+        'msg' => '',
+        'data' => []
+    ];
+
+   // SQL查詢
+    $sql = "SELECT ce.*, m.mem_name, c.c_name
+        FROM course_evaluate ce
+        LEFT JOIN member m ON ce.ev_memid = m.mem_id
+        LEFT JOIN course c ON ce.ev_coid = c.c_id";
+    $evaluates = $pdo->query($sql);
+    $evaluateRows = $evaluates->fetchAll(PDO::FETCH_ASSOC);
+
+    // 將查詢結果賦值給返回資料
+    $returnData['data']['list'] = $evaluateRows;
+} catch (Exception $e) {
+    // 捕獲異常並設置錯誤代碼和錯誤信息
+    $returnData['code'] = 10003;
+    $returnData['msg'] = $e->getMessage();
+}
+
+// 將返回資料編碼為JSON並輸出
+echo json_encode($returnData);
+?>
